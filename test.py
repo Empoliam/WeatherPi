734258b5b2a9567d846cd23572e65634e1c9bcbe
Empoliam <liamjwright@btinternet.com>
2016-01-06 16:21:11 +0000

    Reads temperature every second

---
+++ test.py
@@ -0,0 +1,10 @@
+while 1 == 1:
+	tfile = open("/sys/bus/w1/devices/28-0000075c293f/w1_slave") 
+	text = tfile.read() 
+	tfile.close() 
+	secondline = text.split("\n")[1] 
+	temperaturedata = secondline.split(" ")[9] 
+	temperature = float(temperaturedata[2:]) 
+	temperature = temperature / 1000 
+	print temperature
+	sleep(1)
