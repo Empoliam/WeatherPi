4cab53196fdb5ba9b7376ee457a2f025719dc9d9
comp500 <comp500@users.noreply.github.com>
2016-01-26 15:43:32 +0000

    Update logData.sh

--- BashScripts/logData.sh
+++ BashScripts/logData.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
-unparsed=$(cat /sys/bus/w1/devices/28-0000075c293f/w1_slave)
+unparsed="cat /sys/bus/w1/devices/28-0000075c293f/w1_slave"
 test=$($unparsed | head -n 1 | tail -c 4)
-data=$($unparsed | tail -n 1 | tail -c 4)
+data=$($unparsed | tail -n 1 | tail -c 6)
 
 if [ "$test" == "YES" ] 
 	then
