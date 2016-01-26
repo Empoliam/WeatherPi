76926aba6a322e1300dc35aaedb2c1739dc8198f
comp500 <comp500@users.noreply.github.com>
2016-01-26 15:40:00 +0000

    Update logData.sh

--- BashScripts/logData.sh
+++ BashScripts/logData.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-unparsed=cat /sys/bus/w1/devices/28-0000075c293f/w1_slave
+unparsed=$(cat /sys/bus/w1/devices/28-0000075c293f/w1_slave)
 test=$($unparsed | head -n 1 | tail -c 4)
 data=$($unparsed | tail -n 1 | tail -c 4)
 
