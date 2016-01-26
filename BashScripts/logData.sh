78bd4c87d70de53641aae384e2a21803fd2a98ac
comp500 <comp500@users.noreply.github.com>
2016-01-26 16:02:28 +0000

    Update logData.sh

--- BashScripts/logData.sh
+++ BashScripts/logData.sh
@@ -1,5 +1,5 @@
 unparsed=$(cat /sys/bus/w1/devices/28-0000075c293f/w1_slave)
-test=$($unparsed | head -n 1 | tail -c 4)
+testdata=$($unparsed | head -n 1 | tail -c 4)
 data=$($unparsed | tail -n 1 | tail -c 6)
 if [ "$test" == "YES" ]; then
   "echo" "-e" "Data received."
