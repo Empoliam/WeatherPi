966f70545386204307eae5f9865c0c35022e81d4
Empoliam <liamjwright@btinternet.com>
2016-01-26 16:07:02 +0000

    Please work again

--- Java/src/Test.java
+++ Java/src/Test.java
@@ -1,10 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class Test {
+public class Test 
+{
 
-	public static void main(String[] args) {
-
-		System.out.println("Git is stupid");
-
+	public static void main(String[] args) 
+	{
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("/sys/bus/w1/devices/28-0000075c293f/w1_slave"));
+			System.out.println(br.readLine());
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
